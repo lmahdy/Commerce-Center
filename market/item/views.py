69bd359a7a0071ad8481ef67ddf4a2aa43739e2a
lmahdy <mahdi.mzoughi@istic.ucar.tn>
2024-08-19 14:35:41 +0100

    Edit items

--- market/item/views.py
+++ market/item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 # Create your views here.
@@ -31,6 +31,25 @@ def new(request):
     return render(request, 'item/form.html', {
         'form': form,
         'title': 'New item',
+    })
+
+@login_required
+def edit(request, pk):#one cas submit;two cas get
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)#else if the request is not a POST request, we will create a new instance of the EditItemForm class and pass it to the template means the user will see a form filled with the item's current data if we dont do that when the user clicks on the edit button, they will see a blank form to fill out and submit because we are not passing the instance of the item to the form and we mean by else when the user clicks on the edit button, they will see a form filled with the item's current data the other case is when the user submits the form, we will create a new instance of the EditItemForm class and pass it to the template
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item',
     })
 
 @login_required
